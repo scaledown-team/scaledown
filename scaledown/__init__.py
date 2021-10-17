9d0fbd9614285e2c9723e286e9ffc41e33330b26
Soham Chatterjee <96soham96@gmail.com>
2021-10-17 17:38:29 +0800

    some comments

--- scaledown/__init__.py
+++ scaledown/__init__.py
@@ -2,6 +2,7 @@ from .loader import load_model
 from . import converter
 from . import quantization
 
+# Version of the package. Change here to update version everywhere else
 __version__="0.0.3"
 
 __all__ = [
